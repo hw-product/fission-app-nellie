e978066cd831ab7fb6a96f28bf8db9deae077045
Chris Roberts <chrisroberts.code@gmail.com>
2015-02-02 12:30:52 -0800

    Add initial content for nellie dashboard

--- app/controllers/nellie/dashboard_controller.rb
+++ app/controllers/nellie/dashboard_controller.rb
@@ -1,7 +1,32 @@
 class Nellie::DashboardController < ApplicationController
 
   def index
-
+    respond_to do |format|
+      format.js do
+        flash[:error] = 'Unsupported request!'
+        javascript_redirect_to dashboard_path
+      end
+      format.html do
+        @dataset = Job.dataset_with(
+          :scalars => {
+            :repository_name => [:data, :github, :repository, :full_name],
+            :status => [:status]
+          }
+        ).where(:account_id => @account.id)
+        @data = Smash[
+          @product.repositories_dataset.where(:account_id => @account.id).all.map do |repo|
+            [
+              repo.name,
+              Smash.new(
+                :complete => @dataset.where(:repository_name => repo.name, :status => 'complete'),
+                :error => @dataset.where(:repository_name => repo.name, :status => 'error'),
+                :in_progress => @dataset.where(:repository_name => repo.name, :status => 'in_progress')
+              )
+            ]
+          end
+        ]
+      end
+    end
   end
 
 end
