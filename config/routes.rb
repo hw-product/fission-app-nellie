798fd7e8c33b4809744cd2e190eeacf3bb6d3db6
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-04 12:52:05 -0800

    Fully provide customized job routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
     &FissionApp::Repositories.repositories_routes
   )
 
-  instance_exec(
-    :nellie, :disable_details,
-    &FissionApp::Jobs.jobs_routes
-  )
-
   namespace :nellie do
     get 'dashboard', :to => 'dashboard#index', :as => :dashboard
     get 'job/:job_id', :to => 'jobs#details', :as => :job
+    get 'jobs', :to => 'jobs#all', :as => :jobs
     resources :repository, :only => [:show]
   end
 end
